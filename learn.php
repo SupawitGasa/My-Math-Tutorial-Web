08901cfed93bd42c78535e989b2ea0f3bcb04d8a
SupawitGasa <supawitmonic@gmail.com>
2026-06-27 15:28:25 +0700

    add-learn.php

---
+++ learn.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Welcome To The Learning Space, <?php echo $_POST["name"] ?></h1>
+    <h3>Username: <?php echo $_POST["name"] ?></h3>
+    <h3>Password: <?php echo $_POST["pwd"] ?></h3>
+</body>
+</html>
