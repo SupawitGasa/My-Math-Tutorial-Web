6bf124fc1382d65d44f0bcbe3ba908610e7478ec
SupawitGasa <supawitmonic@gmail.com>
2026-07-01 21:18:59 +0700

    customize-learn-page-#1

--- learn.php
+++ learn.php
@@ -4,10 +4,31 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link rel="stylesheet" href="style.css">
 </head>
 <body>
-    <h1>Welcome To The Learning Space, <?php echo $_POST["name"] ?></h1>
-    <h3>Username: <?php echo $_POST["name"] ?></h3>
-    <h3>Password: <?php echo $_POST["pwd"] ?></h3>
+    <div class="banner">
+        <h1>The logo</h1>
+    </div>
+    <br><br>
+    <?php  
+
+        $return_str = "";
+
+        if ($_POST["uname"] == "Monique" && $_POST["pwd"] == "123") {
+            $return_str = "Welcome To The Learning Space, ".$_POST["uname"];
+        } else {
+            $return_str = "Invalid Credentials !!";
+        }
+
+    ?>
+    <center>
+    <h1 style="color: white;"><?php echo $return_str ?></h1>
+    <?php
+        if ($return_str == "Invalid Credentials !!") {
+            echo '<a href="login.html"><button style="background-color: red; color: white; width: 150px;"><b>Log In</b></button></a>';
+        }
+    ?>
+    </center>
 </body>
 </html>
